0d9f8379a275e05bd96492693380bd1639051032
Jan Herscheid <jan@Jans-MacBook-Pro.local>
2025-10-23 01:14:40 +0200

    add oil

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,24 +14,37 @@ vim.o.wrap = false
 
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>=', vim.lsp.buf.format)
 
+-- plugin section
 vim.pack.add({
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
 	{ src = 'https://github.com/zenbones-theme/zenbones.nvim' },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/seblyng/roslyn.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/iamcco/markdown-preview.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.icons" },
 })
 
 require "mini.pick".setup()
 require "mason".setup()
+require "oil".setup()
+require "mini.icons".setup()
 
+
+-- keymap section
 vim.keymap.set('n', '<leader>ff', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>fs', ":Pick grep live<CR>")
 
-vim.lsp.enable({ "lua_ls", "ts_ls"})
+vim.keymap.set('n', '<leader>fs', ":Pick grep live<CR>")
 
+vim.lsp.enable({ "lua_ls", "ts_ls", "omnisharp"})
+
+vim.keymap.set('n', '<leader>=', vim.lsp.buf.format)
+
+
+-- colourscheme section
 vim.g.zenbones_compat = 1
 vim.cmd("colorscheme zenbones")
 vim.cmd("set background=dark")
